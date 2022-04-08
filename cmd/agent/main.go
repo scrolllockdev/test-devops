43cd7e2cba1b83556f1a7ee04e765634578eb7ca
clownfish <AA@mail.com>
2022-04-08 03:17:04 -0700

    aaaa

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,148 +1,28 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
-	"math/rand"
-	"net/http"
-	"net/url"
 	"os"
-	"path"
-	"runtime"
-	"time"
+	"os/signal"
+	"syscall"
+
+	"github.com/andrei-cloud/go-devops/internal/agent"
+	"github.com/andrei-cloud/go-devops/internal/collector"
 )
 
-func randomValue() gauge {
-	return gauge(rand.Float64())
-}
-
-func updateMetricURL(serverAddress string) string {
-
-	resultURL := url.URL{
-		Scheme: "http",
-		Host:   serverAddress,
-		Path:   path.Join("update"),
-	}
-	return resultURL.String() + "/"
-}
-
-func sendMetric(metric []byte, client *http.Client, cfg Config, ctx context.Context) error {
-	endpoint := updateMetricURL(cfg.ServerAddress)
-	fmt.Printf("Отправка метрик на %s\n", endpoint)
-	request, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBuffer(metric))
-	if err != nil {
-		fmt.Printf("Error while reading %s: %v", endpoint, err)
-		return err
-	}
-	request.Header.Set("Content-Type", "application/json")
-
-	response, err := client.Do(request)
-	if err != nil {
-		fmt.Printf("Error while sending request %s: %v \n", endpoint, err)
-		fmt.Println(response)
-		return err
-	}
-	defer response.Body.Close()
-
-	fmt.Printf("Статус-код %s\n", response.Status)
-
-	return nil
-
-}
-func convertCounterMetricToJSON(name, mType string, value counter) []byte {
-	mValue := int64(value)
-
-	metric := Metrics{
-		ID:    name,
-		MType: mType,
-		Delta: &mValue,
-	}
-
-	metricJSON, err := json.Marshal(metric)
-	if err != nil {
-		fmt.Println("something wrong with marshal", err)
-		panic(err)
-	}
-
-	return metricJSON
-}
-func convertGaugeMetricToJSON(name, mType string, value gauge) []byte {
-	mValue := float64(value)
-
-	metric := Metrics{
-		ID:    name,
-		MType: mType,
-		Value: &mValue,
-	}
-
-	metricJSON, err := json.Marshal(metric)
-	if err != nil {
-		fmt.Println("something wrong with marshal", err)
-		panic(err)
-	}
-
-	return metricJSON
-}
-
 func main() {
+	collector := collector.NewCollector()
+	agent := agent.NewAgent(collector, nil)
 
-	var cfg Config
-	err := cfg.readConfig()
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
+	ctx, cancel := context.WithCancel(context.Background())
 
-	fmt.Println(cfg)
+	go agent.Run(ctx)
 
-	client := &http.Client{}
-	transport := &http.Transport{}
-	transport.MaxIdleConns = 60
-	client.Transport = transport
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	<-sig
 
-	ctx, _ := context.WithCancel(context.Background())
-
-	pollTicker := time.NewTicker(cfg.PollInterval)
-	defer pollTicker.Stop()
-
-	reportTicker := time.NewTicker(cfg.ReportInterval)
-	defer reportTicker.Stop()
-
-	done := make(chan bool)
-
-	var metrics MetricsStorage
-	metrics.counterMetricsStorage = make(map[string]counter)
-	metrics.gaugeMetricsStorage = make(map[string]gauge)
-	metricsCounter := 0
-	for {
-		select {
-		case <-done:
-			fmt.Println("Done!")
-			return
-		case t := <-pollTicker.C:
-			var rtm runtime.MemStats
-			runtime.ReadMemStats(&rtm)
-			metricsCounter++
-			metrics.addMetrics(rtm, randomValue(), counter(metricsCounter))
-			fmt.Printf("Метрики собраны в %s\n", t)
-		case <-reportTicker.C:
-			metricsCounter = 0
-			for name, value := range metrics.gaugeMetricsStorage {
-				if err := sendMetric(convertGaugeMetricToJSON(name, "gauge", value), client, cfg, ctx); err != nil {
-					fmt.Println(err, string(convertGaugeMetricToJSON(name, "gauge", value)))
-				} else {
-					fmt.Println("Success send metric", string(convertGaugeMetricToJSON(name, "gauge", value)))
-				}
-			}
-			for name, value := range metrics.counterMetricsStorage {
-				if err := sendMetric(convertCounterMetricToJSON(name, "counter", value), client, cfg, ctx); err != nil {
-					fmt.Println("Success send metric", string(convertCounterMetricToJSON(name, "counter", value)))
-					fmt.Println(err)
-				}
-			}
-		}
-	}
+	cancel()
+	fmt.Println("agent quit")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,49 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"net/http"
+	"context"
+
+	"github.com/andrei-cloud/go-devops/internal/server"
 )
 
 func main() {
+	s := server.NewServer()
 
-	var cfg Config
-	cfg.readConfig()
+	ctx, cancel := context.WithCancel(context.Background())
+	s.Run(ctx)
 
-	metricsStorage := &Storage{Storage: make([]Metrics, 0), config: cfg}
-
-	if metricsStorage.config.Restore {
-		err := metricsStorage.readFromDbFile()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	if metricsStorage.config.StoreInterval != 0 {
-		go metricsStorage.storeWithInterval()
-	}
-
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(gzipHandle)
-
-	fmt.Println(cfg)
-
-	r.Post("/update/{type}/{name}/{value}", metricsStorage.updateMetric)
-	r.Get("/value/{MetricType}/{MetricName}", metricsStorage.currentMetric)
-	r.Get("/", metricsStorage.allMetrics)
-	// 4 - increment
-	r.Post("/update/", metricsStorage.updateMetricFromBody)
-	r.Post("/value/", metricsStorage.metricValue)
-
-	if err := http.ListenAndServe(metricsStorage.config.ServerAddress, r); err != nil {
-		fmt.Println(err)
-	}
+	s.Shutdown() //blocking function
+	cancel()
 
 }
+
+
+github.com/scrolllockdev/test-devops
